e9fbc3d9bb2092f40dbe8a8d20ffbcb1d4175f8d
Vanessa <vanessa@pebbleroad.com>
2019-10-31 18:34:55 +0800

    Render data from current week, show nav accordingly

--- script.js
+++ script.js
@@ -3,7 +3,20 @@
         $week: document.querySelector(".week"),
         start: function() {
             habit.loadData(function(data) {
-                habit.render(data);
+                const currentWeekData = habit.getCurrentWeekData(data);
+
+                let showPrevBtn = true, showNextBtn = true;
+                if (currentWeekData.week === data[0].week) {
+                    showPrevBtn = false;
+                    showNextBtn = true;
+                }
+                else if (currentWeekData.week === data[data.length-1].week) {
+                    showPrevBtn = true;
+                    showNextBtn = false;
+                }
+                    
+
+                habit.render(currentWeekData, showPrevBtn, showNextBtn);
             });
         },
         loadData: function(fn) {
@@ -11,11 +24,23 @@
                 .then(res => res.json())
                 .then(response => fn(response));
         },
-        render: function(data) {
-            console.log("data2 :", data);
-            habit.$week.innerText = `Week ${data[0].week}`;
+        getCurrentWeekData: function(data) {
+            let currentWeekData = data[0];
+            const today = new Date();
 
-            const weekData = data[0];
+            data.forEach(d => {
+                let startDate = new Date(d.startDate),
+                    endDate = new Date(d.endDate);
+
+                if (today > startDate && today < endDate) {
+                    currentWeekData = d;
+                }
+            })
+
+            return currentWeekData;
+        },
+        render: function(weekData, showPrevBtn, showNextBtn) {
+            habit.$week.innerText = `Week ${weekData.week}`;
 
             function createActionEl({ typeId, title, desc }) {
                 // Create action item card
@@ -35,6 +60,7 @@
                 return action;
             }
 
+            // Render activities
             document.querySelectorAll(".activity_action").forEach($el => {
                 if ($el.id === "saturday" || $el.id === "sunday") {
                     weekData[$el.id].forEach(d => {
@@ -44,6 +70,13 @@
                     $el.appendChild(createActionEl(weekData[$el.id]));
                 }
             });
+
+            // Hide Prev / Next button
+            const $prevBtn = document.querySelector(".footer-nav.type--prev"),
+                $nextBtn = document.querySelector(".footer-nav.type--next");
+                
+            $prevBtn.style.display = showPrevBtn ? 'block' : 'none';
+            $nextBtn.style.display = showNextBtn ? 'block' : 'none';
         }
     };
 
